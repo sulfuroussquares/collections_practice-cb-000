d71ca969a0f97720594a0fa9c4539ce0556defa9
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-07-08 00:08:36 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,15 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort!{|a,b| b<=>a}
+end
+
+def sort_array_char_count(array)
+  array.sort {|a,b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+  array[0], array[3] = array[3], array[0]
+end
